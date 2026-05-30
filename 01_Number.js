4ebb0258daf1ab7ff181450d6c0669229d1bc15e
Krushnat Salavi <krushnasalavi@gmail.com>
2026-05-30 21:11:46 +0530

    Number and its Methodes

---
+++ 01_Number.js
@@ -0,0 +1,14 @@
+const num = 23.12;
+console.log(num);
+
+const aNum = new Number(55);
+console.log(aNum);
+
+console.log(aNum.toString());
+
+console.log(typeof aNum); 
+console.log(num.toPrecision(1)); 
+
+console.log(num.toFixed(1));
+
+console.log(num.toExponential(1));
