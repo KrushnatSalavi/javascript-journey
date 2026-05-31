e8bce65b95c579b7ddf7acc9cfe7727d9f204f8a
Krushnat Salavi <krushnasalavi@gmail.com>
2026-05-31 11:35:47 +0530

    Start Practicing

---
+++ Basic_JavaScriptConcept/03_Function.js
@@ -0,0 +1,12 @@
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+myFunction(); 
+
+const Addition = function(Num1,Nnum2){
+    sum = Num1 + Nnum2;
+    console.log(sum);   
+}
+
+Addition(12,4);  
