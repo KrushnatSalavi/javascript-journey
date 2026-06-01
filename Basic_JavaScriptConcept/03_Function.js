78eae885f14805e4a79a35dff2bbd0d9ad98414c
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-01 19:29:46 +0530

    Arrow , Rest parameter function

--- Basic_JavaScriptConcept/03_Function.js
+++ Basic_JavaScriptConcept/03_Function.js
@@ -1,12 +1,50 @@
-const myFunction = function() {
-    console.log("Hello World");
-}
+// const myFunction = function() {
+//     console.log("Hello World");
+// }
 
-myFunction(); 
+// myFunction(); 
 
-const Addition = function(Num1,Nnum2){
-    sum = Num1 + Nnum2;
-    console.log(sum);   
-}
+// const Addition = function(Num1,Nnum2){
+//     sum = Num1 + Nnum2;
+//     console.log(sum);   
+// }
 
-Addition(12,4);  
+// Addition(12,4);  
+
+// function User(userName){
+//     return `Hello ${userName}`;
+
+// }
+
+// console.log(User("John"));
+
+// // Arrow Function
+// const ArrowFunction = () => {
+//     console.log("Hello from Arrow Function");
+// }
+// ArrowFunction();    
+
+// const ArrowAddition = (Num1,Nnum2) => {
+//     sum = Num1 + Nnum2;
+//     console.log(sum);   
+// }   
+
+// ArrowAddition(10,5);
+
+// // Short Arrow Function
+// const ShortArrowFunction = () => console.log("Hello from Short Arrow Function");
+// ShortArrowFunction();
+
+// // Rest Parameters
+
+// // Accept unlimited arguments
+
+const Rst = function(...numbers){
+    let sum = 0;
+    for (let num in numbers ) {
+        sum += numbers[num];
+    }
+    console.log(sum);
+    }
+
+Rst(1,2,3,4,5);
