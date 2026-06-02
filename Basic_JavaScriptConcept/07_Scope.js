fb5b52efaef05ff34bb3de8e04f7348acb323e89
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-02 21:38:35 +0530

    Scope & Execution Context

---
+++ Basic_JavaScriptConcept/07_Scope.js
@@ -0,0 +1,43 @@
+//globla scope
+
+const name = "Krushna";
+
+function greet() {
+    console.log(name);
+}
+
+greet();
+
+//Function Scope 
+
+function test() {
+    let message = "Hello";
+    console.log(message);
+}
+
+test();
+
+//Block Scope 
+
+{
+    let age = 21;
+}
+
+console.log(age); //ReferenceError: age is not defined
+
+//Lexical Scope
+
+//Functions can access variables from their parent scope.
+
+function outer() {
+
+    let username = "Krushna";
+
+    function inner() {
+        console.log(username);
+    }
+
+    inner();
+}
+
+outer();
