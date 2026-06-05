080104118986934a44f4089280382e913c63d5b2
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-05 11:05:57 +0530

    Prototype in JS

---
+++ Basic_JavaScriptConcept/11_Prototypes/11_Prototype.js
@@ -0,0 +1,19 @@
+const user = {
+    name: "Krushna"
+};
+
+console.log(
+    Object.getPrototypeOf(user)
+);
+
+const arr = [1, 2, 3];
+
+console.log(
+    Object.getPrototypeOf(arr)
+);
+
+// String Prtotype
+
+const name = "Krushna";
+
+console.log(name.toUpperCase());
