5c9f65e5ee888e89f0cf9aa99d49bffabc33ad9c
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-07 17:59:41 +0530

    Practice

--- Basic_JavaScriptConcept/12_OOP/12_Inheritance.js
+++ Basic_JavaScriptConcept/12_OOP/12_Inheritance.js
@@ -1,1 +1,24 @@
 //Inheritance in JavaScript
+
+class Person {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log(
+            `Hello ${this.name}`
+        );
+    }
+
+}
+
+class Student extends Person {
+
+    constructor(name, age) {
+        super(name);
+        this.age = age;
+    }
+
+}
