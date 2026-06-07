df9b0acca666b86733a07a390544e9e5f3382678
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-07 17:02:42 +0530

    Classes in JS

---
+++ Basic_JavaScriptConcept/12_OOP/12_Classes.js
@@ -0,0 +1,15 @@
+//Classes in JavaScript
+
+class Student {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+const student1 =
+    new Student("Krushna", 21);
+
+console.log(student1);
