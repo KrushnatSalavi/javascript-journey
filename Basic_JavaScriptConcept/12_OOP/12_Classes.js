c27fd728833cf15925b90fa4a4b366f9cf24ebd5
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-07 17:55:08 +0530

    Methods in Classes

--- Basic_JavaScriptConcept/12_OOP/12_Classes.js
+++ Basic_JavaScriptConcept/12_OOP/12_Classes.js
@@ -13,3 +13,22 @@ const student1 =
     new Student("Krushna", 21);
 
 console.log(student1);
+
+//Methods in Classes
+
+class Student1 {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }   
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+
+}
+
+const student =
+    new Student1("Krushna", 21);
+    student.greet();
