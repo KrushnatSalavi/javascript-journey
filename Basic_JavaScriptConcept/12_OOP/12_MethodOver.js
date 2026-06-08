8113247ff635d0e3366e9ef8022962a115b92d88
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-08 11:16:55 +0530

    Practice

--- Basic_JavaScriptConcept/12_OOP/12_MethodOver.js
+++ Basic_JavaScriptConcept/12_OOP/12_MethodOver.js
@@ -19,3 +19,16 @@ const s = new Student();
 
 s.greet();
 
+// Using super keyword to call the parent class method
+class Teacher extends Person {
+
+    greet() {
+        super.greet();
+        console.log(
+            "Hello Teacher"
+        );
+    }
+}
+
+const t = new Teacher();    
+t.greet();
