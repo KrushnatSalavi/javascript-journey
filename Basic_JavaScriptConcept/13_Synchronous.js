3f33e9f33351fb3777c601bc2ab95ef6b1a44903
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-09 10:26:59 +0530

    synchronous JavaScript

---
+++ Basic_JavaScriptConcept/13_Synchronous.js
@@ -0,0 +1,22 @@
+console.log("A");
+console.log("B");
+console.log("C");
+// Output will be in order
+//  A, B, C because JavaScript is synchronous and executes code line by line.
+
+
+
+
+// Problem with Synchronous Code
+
+
+console.log("Start");
+
+// Takes 5 seconds
+heavyTask();
+
+console.log("End");
+
+// Start
+// (wait 5 seconds)
+// End
