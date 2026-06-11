f89d2eb4efc177bb53ca227005b01664ff62c9f9
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-11 16:47:42 +0530

    Practice

--- Basic_JavaScriptConcept/Advance_Array_Meth/02_filter.js
+++ Basic_JavaScriptConcept/Advance_Array_Meth/02_filter.js
@@ -7,3 +7,16 @@ const evenNumbers =
 
 console.log(evenNumbers);
 
+// Real world example
+
+const users = [
+    { name: "A", active: true },
+    { name: "B", active: false },
+    { name: "C", active: true }
+];
+
+const activeUsers =
+    users.filter(user => user.active);
+
+console.log(activeUsers);
+
