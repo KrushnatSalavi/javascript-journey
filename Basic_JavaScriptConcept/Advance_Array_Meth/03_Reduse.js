c6c30b9a4e210fca1101fbbb8dbafeef6b44d2e9
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-12 08:54:11 +0530

    Practice Example

--- Basic_JavaScriptConcept/Advance_Array_Meth/03_Reduse.js
+++ Basic_JavaScriptConcept/Advance_Array_Meth/03_Reduse.js
@@ -2,3 +2,12 @@ const myarr = [10,12,13,14,15];
 
 const sum = myarr.reduce( (acc, curr)=>acc+curr, 0 );
 console.log(sum);
+
+let myCart = [
+    {productId: 1, productName: "mobile", price: 12000},
+    {productId: 2, productName: "laptop", price: 22000},
+    {productId: 3, productName: "tv", price: 32000},
+];
+
+const TotalPrc = myCart.reduce((acc, curr) => acc + curr.price, 0);
+console.log(TotalPrc);
