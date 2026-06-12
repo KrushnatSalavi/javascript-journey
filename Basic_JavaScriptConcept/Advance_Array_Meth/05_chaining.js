2cf2606f9188cc0d6ff0c7c10a6a64ddcf8b6d21
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-12 09:23:03 +0530

    Chaining in JS

---
+++ Basic_JavaScriptConcept/Advance_Array_Meth/05_chaining.js
@@ -0,0 +1,25 @@
+numbers = [1, 2, 3, 4, 5];
+
+const result =
+    numbers
+        .filter(num => num % 2 === 0)
+        .map(num => num * 10);
+
+console.log(result);
+
+
+//Functional Programming Mindset
+
+
+let result2 = [];
+
+for(let i=0;i<numbers.length;i++) {
+
+    if(numbers[i] % 2 === 0) {
+        result2.push(numbers[i] * 2);
+    }
+
+}
+console.log(result2);
+
+//Modern JS:
