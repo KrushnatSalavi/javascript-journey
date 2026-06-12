7ba473e1b0e11f8846c0ce793700cd26616db8bd
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-12 09:23:52 +0530

    Practice

--- Basic_JavaScriptConcept/Advance_Array_Meth/05_chaining.js
+++ Basic_JavaScriptConcept/Advance_Array_Meth/05_chaining.js
@@ -23,3 +23,9 @@ for(let i=0;i<numbers.length;i++) {
 console.log(result2);
 
 //Modern JS:
+
+const result3 =
+    numbers
+        .filter(n => n % 2 === 0)
+        .map(n => n * 2);
+console.log(result3);
