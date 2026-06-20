bb06ea017af02ae93e4844566c4ea9acba5751f2
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-20 10:51:18 +0530

    Adding Script to the UI

--- Projects/Color_Changer/script.js
+++ Projects/Color_Changer/script.js
@@ -1,3 +1,19 @@
 const button = document.querySelectorAll('.button')
 const body = document.querySelector("body")
 console.log(body);
+
+button.forEach(function (button) {
+    console.log(button)
+    button.addEventListener('click',function (e) {
+        console.log(e);
+        console.log(e.target)
+        if(e.target.id === 'pick1'){
+            body.style.backgroundColor = 'aquamarine'
+        }
+        
+        }
+
+
+
+    });
+});
