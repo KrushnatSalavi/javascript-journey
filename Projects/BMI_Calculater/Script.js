65b9771a059f658f265f1d01c61bf38211dc7cba
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-20 15:19:27 +0530

    Scripting

--- Projects/BMI_Calculater/Script.js
+++ Projects/BMI_Calculater/Script.js
@@ -1,2 +1,18 @@
 const form = document.querySelector('form')
 
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+   
+    if(height===''|| height < 0 || isNaN(height)){
+        results.innerHTML="! Please Give a Valid height"
+    }
+    else if(weight===''|| weight < 0 || isNaN(weight)){
+        results.innerHTML="! Please Give a Valid height"
+    }
+
+
+})
