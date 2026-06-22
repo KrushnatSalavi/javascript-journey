b04f96428a7395ec8df9af9bf5747e334f5261ae
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-22 09:45:21 +0530

    Scripting

--- Projects/Guess_number_game/script.js
+++ Projects/Guess_number_game/script.js
@@ -13,26 +13,49 @@ let prevGuess = [];
 let numGuess = 1;
 let playGame = true;
 
-if(playGame){
-    
+if (playGame) {
+    submit.addEventListener('click', function (e) {
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        validateGuess(guess)
+    })
 }
 
-function validateGuess(guess){
+function validateGuess(guess) {
+    if (isNaN(guess)) {
+        alert("Enter a valid number")
+    }
+    else if (guess < 1) {
+        alert("Enter a number bigger than 0")
+    }
+    else if (guess > 100) {
+        alert("Enter a number smaller than 100")
+    }
+    else{
+        prevGuess.push(guess);
+        if(numGuess === 11){
+            dispalyGuess(guess)
+            dispalyMessage(`Game Over . Randome number was ${randmNumber}`)
+        }
+    }
+}
+
+function cheakGuess(guess) {
     //
 }
 
-function cheakGuess(guess){
+function dispalyGuess(message) {
     //
 }
 
-function dispalyMessage(message){
+function dispalyMessage(message) {
     //
 }
 
-function endGame(){
+function endGame() {
     //
 }
 
-function NewGame(){
+function NewGame() {
     //
 }
