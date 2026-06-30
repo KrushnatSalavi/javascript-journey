3e585d828c98e9df1f99e85d325120db8f960ea4
Krushnat Salavi <krushnasalavi@gmail.com>
2026-06-30 17:59:36 +0530

    Script for color changing

---
+++ Projects/Unlimited color/script.js
@@ -0,0 +1,31 @@
+// Generate a random color
+
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+
+    for (i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+};
+
+let intervel;
+
+const startChangingcolor = function () {
+    intervel = setInterval(changeBacground, 1000)
+
+    function changeBacground() {
+        document.body.style.backgroundColor = randomColor();
+
+    }
+
+}
+
+const stopChangingcolor = function () {
+    clearInterval(intervel)
+}
+
+document.querySelector('#start').addEventListener('click', startChangingcolor);
+document.querySelector('#stop').addEventListener('click', stopChangingcolor);
