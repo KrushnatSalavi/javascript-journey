9543180d6a406076a0cb836d9931a6aab2264e4d
Krushnat Salavi <krushnasalavi@gmail.com>
2026-07-03 21:23:41 +0530

    Finally in Promise

--- Basic_JavaScriptConcept/17_Adv_Promise.js
+++ Basic_JavaScriptConcept/17_Adv_Promise.js
@@ -1,19 +1,25 @@
- const promiseFour = new Promise( (resolve , reject) => {
-    setTimeout( () =>{
+const promiseFour = new Promise((resolve, reject) => {
+    setTimeout(() => {
         let error = true
-        if(!error){
-            resolve({username : 'krushna', password:123456})
-        }else{
+        if (!error) {
+            resolve({ username: 'krushna', password: 123456 })
+        } else {
             reject('ERROR: Somthing went wrong')
         }
-    },2000)
- })
-
-promiseFour.then((user) =>{
-    console.log(user);
-    return user.username
-}).then( (username) =>{
-    console.log(username)
-}).catch( (error)=>{
-    console.log(error);
+    }, 2000)
 })
+
+promiseFour
+    .then((user) => {
+        console.log(user);
+        return user.username
+    })
+    .then((username) => {
+        console.log(username)
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally( ()=>{
+       console.log("the promise is either resolved or rejected") 
+    })
