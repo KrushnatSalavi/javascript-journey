b2cef5d0a9520e820249ee49b99afc28587d9523
Krushnat Salavi <krushnasalavi@gmail.com>
2026-07-10 11:50:13 +0530

    Notes App Completed

--- Projects/NotesApp/script.js
+++ Projects/NotesApp/script.js
@@ -2,7 +2,15 @@ const notesContainer = document.querySelector(".notes-container");
 const createbtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box")
 
+function showNotes(){
+    notesContainer.innerHTML =localStorage.getItem("notes")
+}
 
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem('notes', notesContainer.innerHTML);
+}
 
 createbtn.addEventListener('click',()=>{
     let inputBox = document.createElement('p');
@@ -13,8 +21,25 @@ createbtn.addEventListener('click',()=>{
     notesContainer.appendChild(inputBox).appendChild(img); 
 })
 
+
 notesContainer.addEventListener('click', (e)=>{
     if(e.target.tagName ==='IMG'){
         e.target.parentElement.remove();
+         updateStorage();
+    }
+    else if(e.target.tagName ==='p'){
+        notes = document.querySelectorAll(".input-box")
+        notes.forEach(nt =>(
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        ))
+    }
+})
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
