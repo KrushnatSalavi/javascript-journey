bb228089b69663ef4caf91fc8d51bff3060efef3
Krushnat Salavi <krushnasalavi@gmail.com>
2026-08-11 09:14:58 +0530

    Create simple server in node.js

--- Advance_Concept/25_server.js
+++ Advance_Concept/25_server.js
@@ -1,5 +1,11 @@
 const http = require('http')
 
 const server = http.createServer((res,req) =>{
+    res.writeHead(200,{'Content-Type':'text/plain'});
+
+})
+
+server.listen(5000,() =>{
+    console.log('server running at http://localhost:500/');
     
 })
